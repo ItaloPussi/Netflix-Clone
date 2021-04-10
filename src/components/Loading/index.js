8d4e3aaaf0635ea40c61928ef5806c0ef7bc1d33
ItaloPussi <italopussi@gmail.com>
2021-04-10 14:40:19 -0300

    Profile selection and loading

---
+++ src/components/Loading/index.js
@@ -0,0 +1,15 @@
+import {Spinner, LockBody, ReleaseBody, Picture} from './styles'
+
+const Loading = ({src, ...restProps}) => {
+    return (
+        <Spinner {...restProps}>
+            <LockBody />
+            <Picture src={`/images/users/${src}.png`} />
+        </Spinner>
+    )
+}
+
+Loading.ReleaseBody = function LoadingReleaseBody(){
+    return <ReleaseBody />
+}
+export default Loading
--- src/containers/Browser.js
+++ src/containers/Browser.js
@@ -1,4 +1,32 @@
+import { useContext, useEffect, useState } from "react";
+import { SelectProfileContainer } from "./Profiles";
+import { FirebaseContext} from '../context/firebase'
+import { Loading, Header } from "../components";
 
 export function BrowserContainer({slides}){
-    return <p>Hello</p>
+    const [profile, setProfile] = useState({})
+    const [loading, setLoading] = useState(true)
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            setLoading(false)
+        },3000)
+    }, [profile.displayName])
+
+    const {firebase} = useContext(FirebaseContext)
+    const user = firebase.auth().currentUser || {}
+
+    return  profile.displayName ? (
+        <>
+            {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+            <Header src="joker1">
+                Hello
+            </Header>
+        </>
+    ) : (
+        <SelectProfileContainer 
+            user={user}
+            setProfile={setProfile}
+        />
+    )
 }
